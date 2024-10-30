66845860839df9c236237fbab26576fa67dd498d
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-10-30 10:10:51 -0400

    Added analytics import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "../styles/globals.css";
 import { UserProvider } from "@/context/UserContext";
+import GoogleAnalytics from "@/components/common/GoogleAnalytics";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
